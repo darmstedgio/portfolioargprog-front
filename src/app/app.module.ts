0a306bfdac34838011f21d41f178d9e0e5263d8b
Darmstedgio <darmstedgio@gmail.com>
2022-07-31 18:57:50 -0300

    Maquetado no dinamico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { TecnologiesComponent } from './tecnologies/tecnologies.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { StudiesComponent } from './studies/studies.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PrincipalComponent,
+    TecnologiesComponent,
+    AboutmeComponent,
+    StudiesComponent,
+    ExperiencesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
