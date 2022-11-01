a9089b2947b14603522a6172880e83cc48a2c2db
Darmstedgio <darmstedgio@gmail.com>
2022-11-01 02:06:15 -0300

    V0.9.1

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -13,8 +13,8 @@ export class LoginComponent implements OnInit {
   isLogged = false;
   isLoginFail = false;
   userLogin: any;
-  public username: string = 'ariel';
-  public password: string = 'aMunoz1';
+  public username: string = '';
+  public password: string = '';
   public errMsj: string = '';
 
   roles: string[] = [];
